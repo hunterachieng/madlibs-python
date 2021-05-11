c893599458a882c5701ee93d3eec4ba9419204fb
hunterachieng <achienghunter@gmail.com>
2021-05-11 09:22:44 +0300

    Madlibs Project

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+
+time_of_day = input("time_of_day e.g morning: ")
+name = input("name: ")
+family_member = input("family_member e.g mother: ")
+wild_animal = input("animal e.g lion: ")
+fruit_name = input("fruit_name e.g berries: ")
+verb = input("verb: ")
+adjective = input("adjective: ")
+
+
+
+
+print(f" It was a cold Sunday {time_of_day} in the village of Omulu. {name} \
+had been asked by her {family_member} to go and pick {fruit_name} in the village's \
+forest. {name} decided to leave in the {time_of_day} before the mountain {wild_animal} \
+returned to their caves. As she was walking through the forest, she heard a sound. \
+{name} quickly turned around and saw a {wild_animal} a few meters away. \
+As the {wild_animal} started to approach her, {name} started {verb} towards her village \
+but the {wild_animal} was too {adjective} and she was eventually caught and devoured on \
+the forest floor.")
+
+
+
